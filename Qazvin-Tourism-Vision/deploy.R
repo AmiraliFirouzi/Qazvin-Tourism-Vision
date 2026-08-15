e0409200453a4e180aed751212c69533fee92e4d
Amirali <heroesmihan@gmail.com>
2026-08-15 16:36:45 -0400

    Delete Qazvin-Tourism-Vision directory

--- Qazvin-Tourism-Vision/deploy.R
+++
@@ -1,8 +0,0 @@
-# Deployment helper
-# Configure rsconnect once for your account, then run:
-#
-# install.packages("rsconnect")
-# library(rsconnect)
-# rsconnect::deployApp()
-#
-# The application directory is this project root.
